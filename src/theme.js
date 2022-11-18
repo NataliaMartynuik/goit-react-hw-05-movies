3215b790535b25c536dc15a5c6269c26fb85cbfe
NataliaMartynuik <98057592+NataliaMartynuik@users.noreply.github.com>
2022-11-18 21:14:01 +0200

    1

--- src/theme.js
+++
@@ -1,9 +0,0 @@
-export const baseTheme = {
-  colors: {
-    primary: '#81183d',
-    secondary: 'rgb(36, 0, 17)',
-    fontPrimary: '#212121',
-    fontSecondary: '#ffffff',
-    shadow: '0px 4px 7px rgba(0, 0, 0, 0,5)',
-  },
-};
