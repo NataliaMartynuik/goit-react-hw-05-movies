58a1e30c05b8bcdc6081cfb580b59cd85a80a20a
NataliaMartynuik <98057592+NataliaMartynuik@users.noreply.github.com>
2022-11-18 20:56:27 +0200

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,20 +2,21 @@ import { Route, Routes, Navigate } from 'react-router-dom';
 import { lazy } from 'react';
 
 
-const Home = lazy(() => import('../pages/Home'));
-const Movies = lazy(() => import('../pages/Movies'));
-const MovieDetails = lazy(() => import('../pages/MovieDetails'));
-const Cast = lazy(() => import('../components/Cast'));
-const Reviews = lazy(() => import('../components/Reviews'));
-const Layout = lazy(() => import('../components/Layout'));
+
+const Home = lazy(() => import('pages/Home'));
+const Movies = lazy(() => import('pages/Movies'));
+const MovieDetails = lazy(() => import('pages/MovieDetails'));
+const Cast = lazy(() => import('components/Cast'));
+const Reviews = lazy(() => import('components/Reviews'));
+const SharedLayout = lazy(() => import('components/SharedLayout'));
 
 
 
-export const App = () => {
+const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route path="/" element={<SharedLayout />}>
           <Route index element={<Home />} />  
           <Route path="movies" element={<Movies />} />
           <Route path="movies/:movieId" element={<MovieDetails />}>
@@ -29,3 +30,5 @@ export const App = () => {
     </>
   );
 };
+
+export default App;
--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -2,14 +2,17 @@ import { Outlet } from 'react-router-dom';
 import { AppBar } from 'components/AppBar/AppBar';
 import { Box } from '../Box/Box.styled';
 import { Suspense } from 'react';
+import { Loader } from 'components/Loader/Loader';
 
 
-export const Layout = () => {
+const SharedLayout = () => {
     return (
         <Box>
             <AppBar />
-            <Suspense fallback="">
+            <Suspense fallback={<Loader />}>
                 <Outlet />
             </Suspense>
         </Box>)
 }
+
+export default SharedLayout;
